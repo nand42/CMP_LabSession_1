1011e61b1d03d50f267dd98e32414d5d8d5644e7
Dario <dario.chinelli95@gmail.com>
2022-10-17 16:34:22 +0200

    commenti al codice

--- main.cpp
+++ main.cpp
@@ -4,32 +4,25 @@
 #include <math.h>
 using namespace std;
 int main () {
-  Vector3d v1;
+  Vector3d v1; // inizialize v1 as instance
   v1.printxyz(); // print
-  v1.setvx(1);
+  v1.setvx(1); // set value single coord
   v1.printxyz(); // print
-  v1.setvy(2);
+  v1.setvy(2); // set value single coord
   v1.printxyz(); // print
-  v1.setvz(3);
-  v1.printxyz(); // print
-  v1.setxyz(3,3,4);
-  v1.printxyz(); // print
+  v1.setvz(3); // set value single coord
+  v1.printxyz(); // print vector cartesian coords
+  // override values
+  v1.setxyz(3,3,4); // set values all coords
+  v1.printxyz(); // print vector coords
   
-  v1.magnitude();
-  v1.printmag(); // print
-  v1.polartheta();
-  v1.polarphi();
+  v1.magnitude(); // calcolate magnitude 
+  v1.printmag(); // print magnitude
+  v1.polartheta(); // calcolate theta angle
+  v1.polarphi(); // calcolate phi angle
   v1.printpolar(); // print
-  v1.convraddeg();
-  v1.printpolar(); // print
+  v1.convraddeg(); // conversion from rad to deg
+  v1.printpolar(); // print vector polar coord
   
   return 0;
 }
-
-/*
-
-Vector3d v(1,2,3);
-
-v.magnitude(1,2,);
-
-*/
